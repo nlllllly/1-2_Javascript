4e177a03bcecb93b7b109f6e42f0c55372d0342c
nlllllly <unfmosp.y14@gmail.com>
2020-07-11 15:29:23 +0900

    [finish]1-2_Javascript

--- app.js
+++ app.js
@@ -1,4 +1,82 @@
 
-var hello = 'Hello World';
+// var hello = 'Hello World';
 
-alert(hello);
+// alert(hello);
+
+
+
+
+// var int1 = 1;
+
+// var int2 = -10;
+
+// var float1 = 3.14;
+
+// var str1 = 'JavaScriptを覚えよう！';
+
+
+
+
+// alert(4 + 3);
+
+// alert(8 - 5);
+
+// alert(2 * 6);
+
+// alert(10 / 2);
+
+
+// alert('Hello' + 'World');
+
+
+
+// var str1 = 'HELLO';
+// var str2 = 'WORLD';
+// alert(str1 + str2);
+
+
+
+// var orange = 100;
+// var apple = 120;
+
+// if (orange < apple) {
+//     alert('みかんの値段がりんごより安い')
+// } else if(orange == apple) {
+//     alert('みかんとりんごが同じ値段') 
+// } else {
+//     alert('みかんの値段がりんごより高い')
+// }
+
+
+// var max = 100;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+//     num = num * 2;
+//     count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+
+
+// var i;
+// var num = 0;
+
+// for(i = 1; i < 11; i++){
+//     num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+
+var sum = (1 + 1);
+var sub = (1 - 1);
+var mul = (1 * 1);
+var div = (1 / 1);
+
+alert('足し算の結果は' + sum);
+alert('引き算の結果は' + sub);
+alert('掛け算の結果は' + mul);
+alert('割り算の結果は' + div);
